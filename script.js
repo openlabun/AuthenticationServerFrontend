0da32654e7a58b60853071836341fd33875781ac
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2024-11-05 07:59:28 -0500

    try again

--- script.js
+++ script.js
@@ -96,9 +96,10 @@ document.getElementById('get-users-form').addEventListener('submit', async (even
             }
         });
 
+
         const users = await response.json();
         if (!response.ok) {
-            throw new Error(result.detail || 'Error fetching users');
+            throw new Error(users.detail || 'Error fetching users');
         }
 
         document.getElementById('users-list').innerText = JSON.stringify(users);
